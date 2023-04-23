70b90f5fcf9e8e8f92b27a3b160179ccf97a01f0
telnet <g04882210@gmail.com>
2023-04-23 02:04:03 +0200

    ouff

--- public/script.js
+++ public/script.js
@@ -1,19 +1,35 @@
-function togglePasswordVisibility() {
-    var passwordInput = document.getElementById("password");
-    var toggleButton = document.querySelector(".toggle-password");
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      toggleButton.innerHTML = "&#128064;";
-    } else {
-      passwordInput.type = "password";
-      toggleButton.innerHTML = "&#128065;";
-    }
+var togglePasswordButton = document.querySelector(".toggle-password img");
+var maxWidth = 30;
+var maxHeight = 20;
+var img = new Image();
+img.onload = function() {
+  var width = img.width;
+  var height = img.height;
+  if (width > maxWidth) {
+    height *= maxWidth / width;
+    width = maxWidth;
   }
+  if (height > maxHeight) {
+    width *= maxHeight / height;
+    height = maxHeight;
+  }
+  togglePasswordButton.style.width = width + "px";
+  togglePasswordButton.style.height = height + "px";
+};
+img.src = togglePasswordButton.src;
 
 
 
 
-  
 
-
-  
+function togglePasswordVisibility() {
+  var passwordField = document.getElementById("password");
+  var togglePasswordButton = document.querySelector(".toggle-password img");
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    togglePasswordButton.src = "img/cooolll.png";
+  } else {
+    passwordField.type = "password";
+    togglePasswordButton.src = "img/cooolll.png";
+  }
+}
