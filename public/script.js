0863a8bf91fe6ede420182476c978196d56a4376
Veris <azoashly@gmail.com>
2023-08-05 19:38:06 +0100

    vcb

--- public/script.js
+++ public/script.js
@@ -1,35 +1,41 @@
-var togglePasswordButton = document.querySelector(".toggle-password img");
-var maxWidth = 30;
-var maxHeight = 20;
-var img = new Image();
-img.onload = function() {
-  var width = img.width;
-  var height = img.height;
-  if (width > maxWidth) {
-    height *= maxWidth / width;
-    width = maxWidth;
-  }
-  if (height > maxHeight) {
-    width *= maxHeight / height;
-    height = maxHeight;
-  }
-  togglePasswordButton.style.width = width + "px";
-  togglePasswordButton.style.height = height + "px";
-};
-img.src = togglePasswordButton.src;
-
-
-
-
-
 function togglePasswordVisibility() {
-  var passwordField = document.getElementById("password");
-  var togglePasswordButton = document.querySelector(".toggle-password img");
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-    togglePasswordButton.src = "img/cooolll.png";
-  } else {
-    passwordField.type = "password";
-    togglePasswordButton.src = "img/cooolll.png";
+    var passwordInput = document.getElementById("password");
+    var toggleButton = document.querySelector(".toggle-password");
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      toggleButton.innerHTML = "&#128064;";
+    } else {
+      passwordInput.type = "password";
+      toggleButton.innerHTML = "&#128065;";
+    }
   }
-}
+
+// On récupère le formulaire
+const form = document.querySelector('form');
+
+// On écoute l'événement "submit" du formulaire
+form.addEventListener('submit', (event) => {
+	// On empêche le comportement par défaut du formulaire (rechargement de la page)
+	event.preventDefault();
+
+	// On récupère les 6 inputs de type "text"
+	const inputs = document.querySelectorAll('input[type="text"]');
+
+	// On vérifie que les 6 inputs sont remplis
+	let code = '';
+	for (let i = 0; i < inputs.length; i++) {
+		if (inputs[i].value === '') {
+			alert('Veuillez remplir tous les champs.');
+			return;
+		}
+		code += inputs[i].value;
+	}
+
+	// On affiche le code récupéré
+	alert(`Le code récupéré est : ${code}`);
+});
+
+  
+
+
+  
